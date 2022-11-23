5072fd5d7d65a1d0f1567ac4469fa1ba6965b878
wahyurhy <wahyurahayu82@gmail.com>
2022-11-24 05:11:10 +0700

    make it better SearchBar with several params

--- app/src/main/java/com/wahyurhy/jetcoffee/ui/components/SearchBar.kt
+++ app/src/main/java/com/wahyurhy/jetcoffee/ui/components/SearchBar.kt
@@ -1,18 +1,51 @@
 package com.wahyurhy.jetcoffee.ui.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.TextField
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.wahyurhy.jetcoffee.ui.theme.JetCoffeeTheme
+import com.wahyurhy.jetcoffee.R
 
 @Composable
 fun SearchBar(
     modifier: Modifier = Modifier,
 ) {
-    TextField(value = "", onValueChange = {}, modifier = modifier.padding(16.dp))
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface,
+            disabledIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        ),
+        placeholder = {
+            Text(text = stringResource(R.string.placeholder_search))
+        },
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .heightIn(min = 48.dp)
+            .clip(RoundedCornerShape(16.dp))
+    )
 }
 
 @Preview(showBackground = true)
